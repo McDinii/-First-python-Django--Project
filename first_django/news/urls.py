464ea78d1fcf31cd110db1f8f0304dd4f7b7cd0e
McDinii <denisnechay@gmail.com>
2022-02-01 21:20:35 +0300

    added on site functions for editing & deleting articles and end this project(maybe, maybe not)

--- first_django/news/urls.py
+++ first_django/news/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [ #тут уже отслеживаем ветку после ../news/
     path('', views.news_home, name='news_home'),
-    path('create', views.news_home, name='create'),
-
+    path('create', views.create, name='create'),
+    path('<int:pk>', views.NewsDetail.as_view(), name = "news-detail"),
+    path('<int:pk>/update', views.NewsUpdateView.as_view(), name = "news-update"),
+    path('<int:pk>/delete', views.NewsDeleteView.as_view(), name = "news-delete"),
 ]
--- first_django/news/views.py
+++ first_django/news/views.py
@@ -1,9 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
-
+from .forms import ArticleForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 def news_home(request):
     news = Article.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news' : news})
+
+class NewsDetail(DetailView):
+    model = Article
+    template_name = "news/details_view.html"
+    context_object_name = "article"
+
+class NewsUpdateView(UpdateView):
+    model = Article
+    template_name = "news/create.html"
+    form_class = ArticleForm
+
+class NewsDeleteView(DeleteView):
+    model = Article
+    success_url = "/news/"
+    context_object_name = "article"
+    template_name = "news/news-delete.html"
+
 def create(request):
-    return render(request, 'news/create.html')
+    error = ''
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid(): #is_valid in ModelForm
+            form.save()
+            return redirect('news_home')
+        else:
+            error = "Форма была не верной"
+
+    form = ArticleForm()
+
+    data = {
+        "form":form,
+        "error" : error,
+    }
+
+    return render(request, 'news/create.html', data)
